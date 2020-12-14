f19bd34453dd9f52a2e50084f01243c82f0fb8bc
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-13 20:02:50 -0600

    Get basic functionality of make_selection down.

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -10,7 +10,7 @@ enum Character { CH_X, CH_O, CH_EMPTY };
 
 void build_game_board(Character game_board[][3]);
 void display_game_board(Character game_board[][3]);
-void make_selection(Character player, int row, int col);
+void make_selection(Character player, int row, int col, Character game_board[][3]);
 std::string convert_character_to_symbol(Character c);
 
 int main() {
@@ -22,14 +22,15 @@ int main() {
     build_game_board(game_board);
     display_game_board(game_board);
 
-    make_selection(player_1, 0, 0);
+    make_selection(player_1, 0, 0, game_board);
+    make_selection(player_2, 1, 1, game_board);
     display_game_board(game_board);
 
     return 0;
 }
 
-void make_selection(Character player, int row, int col) {
-
+void make_selection(Character player, int row, int col, Character game_board[][3]) {
+    game_board[row][col] = player;
 }
 
 std::string convert_character_to_symbol(Character c) {
@@ -50,12 +51,15 @@ std::string convert_character_to_symbol(Character c) {
 }
 
 void display_game_board(Character game_board[][3]) {
+    std::cout << "----------------" << std::endl;
     for (int row = 0; row < NUM_ROWS; row++) {
         for (int col = 0; col < NUM_COLS; col++) {
             std::cout << " | " << convert_character_to_symbol(game_board[row][col]);
         }
         std::cout << " |\n";
     }
+    std::cout << "----------------" << std::endl;
+
 }
 
 void build_game_board(Character game_board[][3]) {
