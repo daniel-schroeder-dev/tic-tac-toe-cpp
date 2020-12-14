d15326426f49739c07258c69cc9e0f1cc26c17e4
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-13 19:37:22 -0600

    Create basic game_board.

---
+++ tic_tac_toe.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int NUM_ROWS = 3;
+int NUM_COLS = 3;
+
+enum Character { CH_X, CH_O, CH_EMPTY };
+
+void build_game_board(Character game_board[][3]);
+
+
+int main() {
+
+    Character game_board[NUM_ROWS][NUM_COLS];
+
+    return 0;
+}
+
+void build_game_board(Character game_board[][3]) {
+    for (int row = 0; row < NUM_ROWS; row++) {
+        for (int col = 0; col < NUM_COLS; col++) {
+            game_board[row][col] = CH_EMPTY;
+        }
+    }
+}
