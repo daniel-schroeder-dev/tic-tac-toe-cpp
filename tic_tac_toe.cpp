dd7f7c87ba12cd5f45d472a99eee62505ac274f0
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-13 19:39:11 -0600

    Add display_game_board function.

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -6,13 +6,26 @@ int NUM_COLS = 3;
 enum Character { CH_X, CH_O, CH_EMPTY };
 
 void build_game_board(Character game_board[][3]);
+void display_game_board(Character game_board);
 
 
 int main() {
 
     Character game_board[NUM_ROWS][NUM_COLS];
 
+    build_game_board(game_board);
+    display_game_board(game_board);
+
     return 0;
+}
+
+void display_game_board(Character game_board) {
+    for (int row = 0; row < NUM_ROWS; row++) {
+        for (int col = 0; col < NUM_COLS; col++) {
+            std::cout << "| " << CH_EMPTY << " |";
+        }
+        std::endl;
+    }
 }
 
 void build_game_board(Character game_board[][3]) {
