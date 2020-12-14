43ccc04e916b1bbec43e384dfd28ef94f7302f19
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-13 19:43:23 -0600

    Check that display_game_board is working nice.

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -6,12 +6,12 @@ int NUM_COLS = 3;
 enum Character { CH_X, CH_O, CH_EMPTY };
 
 void build_game_board(Character game_board[][3]);
-void display_game_board(Character game_board);
+void display_game_board(Character game_board[][3]);
 
 
 int main() {
 
-    Character game_board[NUM_ROWS][NUM_COLS];
+    Character game_board[3][3];
 
     build_game_board(game_board);
     display_game_board(game_board);
@@ -19,12 +19,12 @@ int main() {
     return 0;
 }
 
-void display_game_board(Character game_board) {
+void display_game_board(Character game_board[][3]) {
     for (int row = 0; row < NUM_ROWS; row++) {
         for (int col = 0; col < NUM_COLS; col++) {
             std::cout << "| " << CH_EMPTY << " |";
         }
-        std::endl;
+        std::cout << "\n";
     }
 }
 
