0ef0b0b8fd996e835f838588f787589fe40353a2
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-13 20:44:08 -0600

    Add ability to check for winner on rows.

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -21,6 +21,9 @@ std::string convert_character_to_symbol(Character c);
 
 int prompt_for_selection(std::string message);
 
+Character check_for_winner(Character game_board[][3]);
+Character check_rows(Character game_board[][3]);
+
 int main() {
 
     Character game_board[3][3];
@@ -28,7 +31,7 @@ int main() {
     Character player_2 = CH_O;
     Character current_player = player_1;
     
-    bool should_continue = true;
+    Character winner = CH_EMPTY;
 
     int row;
     int col;
@@ -36,7 +39,7 @@ int main() {
     build_game_board(game_board);
     display_game_board(game_board);
 
-    while (should_continue) {
+    while (winner == CH_EMPTY) {
         display_player_avatar(current_player);
         row = prompt_for_selection("Select row: ");
         col = prompt_for_selection("Select col: ");
@@ -44,10 +47,37 @@ int main() {
         make_selection(current_player, row, col, game_board);
         display_game_board(game_board); 
 
+        winner = check_for_winner(game_board);
+
         current_player = current_player == player_1 ? player_2 : player_1;
     }
 
+    std::cout << get_player_avatar(winner) << " wins!!!" << std::endl;
+
     return 0;
+}
+
+Character check_rows(Character game_board[][3]) {
+    for (int row = 0; row < NUM_ROWS; row++) {
+        bool all_same_character = true;
+        Character first_character = game_board[row][0];
+        for (int col = 1; col < NUM_COLS; col++) {
+            if (game_board[row][col] == CH_EMPTY) {
+                all_same_character = false;
+                break;
+            }
+            if (game_board[row][col] != first_character) {
+                all_same_character = false;
+                break;
+            }
+        }
+        if (all_same_character) return first_character;
+    }
+    return CH_EMPTY;
+}
+
+Character check_for_winner(Character game_board[][3]) {
+    return check_rows(game_board);
 }
 
 int prompt_for_selection(std::string message) {
