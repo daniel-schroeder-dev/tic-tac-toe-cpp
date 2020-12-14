81426c6675bcab8195d4d42261af6fbf2ea84eb9
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-14 11:06:12 -0600

    Remove srand() as not needed anymore.

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -176,7 +176,6 @@ void display_game_board(Character game_board[][3]) {
 }
 
 void build_game_board(Character game_board[][3]) {
-    srand(time(NULL));
     for (int row = 0; row < NUM_ROWS; row++) {
         for (int col = 0; col < NUM_COLS; col++) {
             game_board[row][col] = CH_EMPTY;
